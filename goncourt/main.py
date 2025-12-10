c57f3a11c3f00b825fe5c4374b6394753f62360a
DavidB <david.bernard@fms-ea.com>
2025-12-10 09:38:51 +0100

    first, second and third selectio s display og in main + display of all the books details from user ok

--- goncourt/main.py
+++ goncourt/main.py
@@ -8,82 +8,103 @@ def main(choice: str) -> None:
 
     if choice == 'the president':
         goncourt = Goncourt()
-        print("Bienvenue Président du Goncourt!")
+        print("Welcome President of Goncourt!")
         for book_id in range(1, 16):
-            book = goncourt.get_book_by_id(book_id)
+            book = Goncourt.get_book_by_id(book_id)
             if book is not None:
-                goncourt.add_book_to_books_1(book)
-        all_books = goncourt.get_books_from_books_1()
-        for book in all_books:
-            goncourt.set_book_selection(book, "first")
-        print("Nous sommes le 3 Septembre 2025 et les 15 premiers livres de la sélection ont été dévoilés.")
+                book.selection = "first"
+                goncourt.add_book_to_selection(book)
+        all_books = goncourt.get_books_from_selection("first")
+        print("We are on September 3rd, 2025 and the first 15 books of the first selection have been revealed.")
         print("--------------------------------")
-        print("Voici la liste des livres de la première sélection :")
+        print("Here is the list of books from the first selection:")
         print("--------------------------------")
-        for book_id in range(1, 16):
-            book = goncourt.get_book_by_id(book_id)
-            if book is not None:
-                goncourt.add_book_to_books_1(book)
-        all_books = goncourt.get_books_from_books_1()
         for book in all_books:
             print(book.__str__())
             print("\n")
-
         print("--------------------------------")
-        print("Rendez-vous le 7 Octobre 2025 pour découvrir les livres de la deuxième sélection.")
+        print("See you on October 7th, 2025 to discover the books of the second selection.")
         print("--------------------------------")
         print("\n")
-        print("Bienvenue Président du Goncourt!")
-        print("Nous sommes le 7 Octobre 2025 et les 8 livres de la deuxième sélection ont été dévoilés.")
+        print("Welcome President of Goncourt!")
+        print("We are on October 7th, 2025 and the 8 books of the second selection have been revealed.")
         print("--------------------------------")
+        for book_id in range(1, 9):
+            book = goncourt.get_book_by_id(book_id)
+            if book is not None:
+                book.selection = "second"
+                goncourt.add_book_to_selection(book)
+        all_books = goncourt.get_books_from_selection("second")
+        print("Here is the list of books from the second selection:")
+        for book in all_books:
+            print(book.__str__())
+            print("\n")
+        print("--------------------------------")
+        print("See you on October 28th, 2025 to discover the books of the third selection.")
+        print("--------------------------------")
+        print("\n")
+        print("Welcome President of Gouncourt!")
+        print("We are on October 28th, 2025 and the 4 books of the third selection have been revealed.")
+        print("--------------------------------")
+        for book_id in range(1, 5):
+            book = goncourt.get_book_by_id(book_id)
+            if book is not None:
+                book.selection = "third"
+                goncourt.add_book_to_selection(book)
+        all_books = goncourt.get_books_from_selection("third")
+        print("Here is the list of books from the third selection:")    
+        for book in all_books:
+            print(book.__str__())
+            print("\n")
+        print("--------------------------------")
+        print("See you on November 5th, 2025 to discover the Goncourt 2025 winner.")
+        print("--------------------------------")
+        print("\n")
+        print("Welcome President of Goncourt!")
+        print("We are on November 5th, 2025 and the Goncourt 2025 winner has been revealed.")
+        winner_book = Goncourt.get_book_by_id(1)
+        print("Here is the Goncourt 2025 winning book:")
+        if winner_book is not None:
+            print(winner_book.__str__())
 
-        print("Voici la liste des livres de la deuxième sélection:")
-        selection = input("Quelle sélection voulez-vous consulter (2 ou 3) ? ").strip()
-        if selection not in ['2', '3']:
-            print("Sélection invalide. Veuillez choisir 2 ou 3.")
-            return
-        selection_number = int(selection)
-
+    elif choice == 'user':
         goncourt = Goncourt()
-
-    # elif choice == 'user':
-    #     goncourt = Goncourt()
         
-    #     for book_id in range(1, 16):
-    #         book = goncourt.get_book_by_id(book_id)
-    #         if book is not None:
-    #             goncourt.add_book_to_books_1(book)
-    #     all_books = goncourt.get_books_from_books_1()
-    #     print("--------------------------------")
-    #     print("Books from first selection:")
-    #     print("--------------------------------")
-    #     for book in all_books:
-    #         print(book.__str__())
-    #         print("\n")
+        for book_id in range(1, 16):
+            book = goncourt.get_book_by_id(book_id)
+            if book is not None:
+                goncourt.add_book_to_books_1(book)
+        all_books = goncourt.get_books_from_books_1()
+        print("--------------------------------")
+        print("Books from first selection:")
+        print("--------------------------------")
+        for book in all_books:
+            print(book.__str__())
+            print("\n")
         
-    #     for i in range(1, 9):
-    #         book = goncourt.get_book_by_id(i)
-    #         if book is not None:
-    #             goncourt.add_book_to_books_2(book)
-    #     all_books_2 = goncourt.get_books_from_books_2()
-    #     print("--------------------------------")
-    #     print("Books from second selection:")   
-    #     print("--------------------------------")
-    #     for book in all_books_2:
-    #         print(book.__str__())
-    #         print("\n")
+        for i in range(1, 9):
+            book = goncourt.get_book_by_id(i)
+            if book is not None:
+                goncourt.add_book_to_books_2(book)
+        all_books_2 = goncourt.get_books_from_books_2()
+        print("--------------------------------")
+        print("Books from second selection:")   
+        print("--------------------------------")
+        for book in all_books_2:
+            print(book.__str__())
+            print("\n")
 
-    #     for i in range (1, 5):
-    #         book = goncourt.get_book_by_id(i)
-    #         if book is not None:
-    #             goncourt.add_book_to_books_3(book)
-    #     all_books_3 = goncourt.get_books_from_books_3()
-    #     print("--------------------------------")
-    #     print("Books from third selection:")
-    #     print("--------------------------------")
-    #     for book in all_books_3:
-    #         print(book.__str__())
-    #         print("\n")
+        for i in range (1, 5):
+            book = goncourt.get_book_by_id(i)
+            if book is not None:
+                goncourt.add_book_to_books_3(book)
+        all_books_3 = goncourt.get_books_from_books_3()
+        print("--------------------------------")
+        print("Books from third selection:")
+        print("--------------------------------")
+        for book in all_books_3:
+            print(book.__str__())
+            print("\n")
         
     
 
