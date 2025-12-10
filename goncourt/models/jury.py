578db093b13c7545fdbaef94593c7fa8e839a59e
DavidB <david.bernard@fms-ea.com>
2025-12-10 10:56:14 +0100

    jury.py updated to fit jury_dao.py

--- goncourt/models/jury.py
+++ goncourt/models/jury.py
@@ -1,18 +1,18 @@
 
+from dataclasses import dataclass, field
+from typing import Optional
 
+
+@dataclass
 class Jury:
     """
     Represents the jury's member and his data :
     - first_name : jury member's first name
     - last_name  : jury member's last name
     """
-    def __init__(self, first_name: str, last_name: str):
-        self.first_name = first_name
-        self.last_name = last_name
-
-    def get_full_name(self) -> str:
-        """Returns the complete name of the jury's member."""
-        return f"{self.first_name} {self.last_name}"
+    first_name: str
+    last_name:str
+    jury_id: Optional[int] = field(default=None, init=False)
     
     def __str__(self) -> str:
-        return self.get_full_name()
+        return f"{self.first_name} {self.last_name}"
